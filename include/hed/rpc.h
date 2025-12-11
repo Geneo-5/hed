263f4bf26d83243649e76c8ce3da50b5bba371ed
Loic JOURDHEUIL SELLIN <46419549+Geneo-5@users.noreply.github.com>
2025-12-11 16:50:39 +0100

    DRAFT: add msg ctx

--- include/hed/rpc.h
+++ include/hed/rpc.h
@@ -14,6 +14,7 @@
 struct hed_rpc_msg {
 	struct galv_sess_msg super; /* first */
 	uint32_t id;
+	void * ctx;
 };
 
 typedef int hed_rpc_fn(struct hed_rpc_msg * msg);
@@ -21,10 +22,35 @@ typedef int hed_rpc_fn(struct hed_rpc_msg * msg);
 struct hed_rpc_conn {
 	struct galv_sess_conn super; /* first */
 	const size_t rcp_nb;
-	const hed_rpc_fn * const * const rpc;
+	hed_rpc_fn * const rpc;
 };
 
-static inline void __hed_nonull(1, 2, 3, 4)
+
+static inline struct hed_rpc_msg * __hed_nonull(1)
+hed_rpc_alloc_req(struct hed_rpc_conn * conn, void * ctx)
+{
+	brg_rpc_assert(conn);
+
+	struct hed_rpc_msg *msg;
+
+	msg = glav_sess_alloc_req(&conn->super);
+	if (!msg)
+		return NULL;
+
+	msg->ctx = ctx;
+	return msg;
+}
+
+static inline struct hed_rpc_msg * __hed_nonull(1)
+hed_rpc_alloc_ntfy(struct hed_rpc_conn * conn)
+{
+	brg_rpc_assert(conn);
+
+	return glav_sess_alloc_ntfy(&conn->super);
+}
+
+
+static inline void __hed_nonull(1)
 hed_rpc_drop_msg(struct hed_rpc_msg *msg)
 {
 	glav_sess_drop_msg(&msg->super);
@@ -33,21 +59,21 @@ hed_rpc_drop_msg(struct hed_rpc_msg *msg)
 struct hed_rpc_auth {
 	uint32_t id;
 	gid_t    gid;
-	const hed_rpc_fn * const rpc;
+	hed_rpc_fn * rpc;
 };
 
 struct hed_rpc_accept {
 	struct glav_sess_accept super; /* first */
 	const uint32_t id_max;
-	const size_t rcp_nb;
-	const hed_rpc_auth * const * const rpc;
+	const unsigned int rcp_nb;
+	const struct hed_rpc_auth * const rpc;
 };
 
 struct hed_rpc_accept_conf {
 	struct galv_sess_accept_conf super;
 	uint32_t id_max;
-	size_t   rpc_nb;
-	const hed_sess_rpc_fn * const * const rpc;
+	unsigned int rpc_nb;
+	const struct hed_sess_rpc_fn * rpc;
 };
 
 #define HED_RPC_ACCEPT_CONF(_backlog, _conn_flags, _max_pload, _buff_capa, \
@@ -61,11 +87,11 @@ struct hed_rpc_accept_conf {
 	}
 
 extern int
-hed_rpc_open_accept(struct hed_rpc_accept      * acceptor,
-                    struct glav_repo           * repository,
-                    struct galv_adopt          * adopter,
-                    const struct upoll         * poller,
-                    struct hed_rpc_accept_conf * conf)
+hed_rpc_open_accept(struct hed_rpc_accept            * acceptor,
+                    struct glav_repo                 * repository,
+                    struct galv_adopt                * adopter,
+                    const struct upoll               * poller,
+                    const struct hed_rpc_accept_conf * conf)
 	__hed_nonull(1, 2, 3, 4, 5);
 
 #endif /* _HED_RPC_H */
--- lib/rpc.c
+++ lib/rpc.c
@@ -14,6 +14,7 @@ hed_rpc_xfer(struct galv_sess_conn * ctx)
 	struct hed_rpc_conn  *conn = (struct hed_rpc_conn *)ctx;
 	struct hed_rpc_msg   *msg;
 	struct dpack_decoder  decoder;
+	uint32_t              id;
 	int                   ret = 0;
 
 	while(galv_sess_may_pull_msg(conn)) {
@@ -21,22 +22,31 @@ hed_rpc_xfer(struct galv_sess_conn * ctx)
 		hed_assert_intern(msg);
 
 		hed_decoder_init(&decoder, msg);
-		ret = dpack_decode_uint32(&decoder, &msg->id);
+		ret = dpack_decode_uint32(&decoder, &id);
 		hed_decoder_fini(&decoder);
-		if (ret)
-			goto error;
+		if (ret) {
+			hed_rpc_drop_msg(msg);
+			break;
+		}
+
+		if (hed_rpc_msg_get_type(msg) == GALV_SESS_HEAD_REPLY_TYPE) {
+			if (msg->id != id) {
+				ret = -EINVAL;
+				hed_rpc_drop_msg(msg);
+				break;
+			}
+
+		} else {
+			msg->id = id;
+		}
 
 		if ((msg->id >= conn->rpc_nb) || (conn->rpc[msg->id] == NULL) {
 			ret = -EPERM;
-			goto error;
+			hed_rpc_drop_msg(msg);
+			break;
 		}
 
 		ret = conn->rpc[msg->id](msg);
-		if (!ret)
-			continue;
-
-error:
-		hed_rpc_drop_msg(msg);
 		if (ret)
 			break;
 	}
@@ -62,11 +72,11 @@ hed_rpc_open_accept(struct hed_rpc_accept      * acceptor,
 
 	int ret;
 
-	ret = galv_sess_open_accept(&acceptor->super, \
-	                            &hed_rpc_ops, \
-	                            repository, \
-	                            adopter, \
-	                            poller, \
+	ret = galv_sess_open_accept(&acceptor->super,
+	                            &hed_rpc_ops,
+	                            repository,
+	                            adopter,
+	                            poller,
 	                            &conf->super);
 	if (ret)
 		return ret;
